7a15ec3be37699044cfa0973f2996f720d7cc774
sajidahw <sajidah@gmail.com>
2024-11-27 22:08:57 -0800

    updated vite.js outDir

--- petastic_adoption/client/vite.config.js
+++ petastic_adoption/client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "dist",
+    outDir: "dist/client/",
   },
   plugins: [react()],
 });
