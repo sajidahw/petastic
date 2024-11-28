ba3dc4d4b7b9a6026102ffed9f9caefab8bd936f
sajidahw <sajidah@gmail.com>
2024-11-27 22:15:30 -0800

    commented out build in vite.config

--- petastic_adoption/client/vite.config.js
+++ petastic_adoption/client/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  build: {
-    outDir: "/server/public",
-  },
+  // build: {
+  //   outDir: "",
+  // },
   plugins: [react()],
 });
