aac06d11c95b2ab2b22d6107e8dadce1676bcc40
sajidahw <sajidah@gmail.com>
2025-01-18 15:36:02 -0800

    /pet/:id

--- petastic_adoption/server/routes/pet.route.js
+++ petastic_adoption/server/routes/pet.route.js
@@ -18,14 +18,14 @@ const router = express.Router();
 router.get("/pets", getPets); // pets gallery page
 
 // GET: Fetch a pet by id, /pets/:id; should /pet/:id; working as "/:id"
-router.get("/pets/:id", getPetById); // fetching a pet by id
+router.get("/pet/:id", getPetById); // fetching a pet by id
 
 // POST: CREATE a new pet, /pets; should /pet/:id
 router.post("/pet", createPet); // creating a pet
 // router.post("/:id", createPet);
 
 // PUT: UPDATE a pet by id, /pets/:id, should /pet/:id
-router.put("/pets/:id", updatePet); // updating a pet
+router.put("/pet/:id", updatePet); // updating a pet
 
 // DELETE: DELETE a pet by id by destructuring id from req.params, /pets/:id; should /pet/:id
 router.delete("/pet/:id", deletePet); // deleting a pet
