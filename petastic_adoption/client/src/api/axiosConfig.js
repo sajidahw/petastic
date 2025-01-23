e21a4326af95014796213d291ee804c03aa1d9d3
sajidahw <sajidah@gmail.com>
2025-01-22 16:14:44 -0800

    reverted back to localhost

--- petastic_adoption/client/src/api/axiosConfig.js
+++ petastic_adoption/client/src/api/axiosConfig.js
@@ -2,8 +2,7 @@ import axios from "axios";
 // for db adoption, backend endpoints use /api/pets
 
 const axiosC = axios.create({
-  baseURL:
-    import.meta.env.VITE_APP_API_URL || "https://petastic.vercel.app/api",
+  baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:8181/api",
   headers: {
     "Content-Type": "application/json",
   },
--- petastic_adoption/client/vite.config.js
+++ petastic_adoption/client/vite.config.js
@@ -11,13 +11,13 @@ export default defineConfig({
   server: {
     proxy: {
       "/": {
-        target: "https://petastic.vercel.app", // Your backend server URL, http://localhost:8181
+        target: "http://localhost:8181", // Your backend server URL, http://localhost:8181
         changeOrigin: true, // Required for CORS to work
         secure: true, // If your backend uses HTTPS with a self-signed certificate, set to false
         // rewrite: (path) => path.replace(/^\/api/, ""), // Optional: remove the /api prefix when forwarding the request
       },
       "/api": {
-        target: "https://petastic.vercel.app", // Your backend server URL, http://localhost:8181
+        target: "http://localhost:8181", // Your backend server URL, http://localhost:8181
         changeOrigin: true, // Required for CORS to work
         secure: true, // If your backend uses HTTPS with a self-signed certificate, set to false
         rewrite: (path) => path.replace(/^\/api/, ""), // Optional: remove the /api prefix when forwarding the request
