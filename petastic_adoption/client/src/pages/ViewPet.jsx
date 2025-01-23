9bbf21bf6648d6b3086c9bcbf33e514bead75bc6
sajidahw <sajidah@gmail.com>
2025-01-23 15:32:32 -0800

    testing

--- petastic_adoption/client/src/pages/ViewPet.jsx
+++ petastic_adoption/client/src/pages/ViewPet.jsx
@@ -45,9 +45,9 @@ import axios from "../api/axiosConfig.js";
 // } from "../../server/controllers/pet.controller.js";
 
 // ViewPet component to display recently added pet details
-const ViewPet = ({ petData }) => {
+const ViewPet = () => {
   const { id } = useParams(); // matching route definition as a String type
-  const [{petData}, setPetData] = useParams();
+  const [petData, setPetData] = useState(null);
   // const [petData, setPetData] = useState(null); //single pet object data; NOT list
   const navigate = useNavigate();
   // const base_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:8181";
